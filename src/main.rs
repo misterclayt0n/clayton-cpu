67fcf64acf297798de89e3a1487b5a026ccdaa86
misterclayt0n <daviarantes13@gmail.com>
2024-05-24 11:18:28 -0300

    refactor program for greater usability

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,20 @@ struct CPU {
 }
 
 impl CPU {
+    fn new() -> CPU {
+        CPU {
+            registers: [0; 16],
+            memory: [0; 4096],
+            position_in_memory: 0,
+            stack: [0; 16],
+            stack_pointer: 0,
+        }
+    }
+
+    fn load_program(&mut self, program: &[u8], start_address: usize) {
+        self.memory[start_address..(start_address + program.len())].copy_from_slice(program);
+    }
+
     fn read_opcode(&self) -> u16 {
         let p = self.position_in_memory;
         let op_byte1 = self.memory[p] as u16;
@@ -30,7 +44,7 @@ impl CPU {
 
             match (c, x, y, d) {
                 (0, 0, 0, 0) => {
-                    println!("Terminating execution.");
+                    println!("terminating execution.");
                     return;
                 }
                 (0, 0, 0xE, 0xE) => self.ret(),
@@ -62,7 +76,7 @@ impl CPU {
         let stack = &mut self.stack;
 
         if sp > stack.len() {
-            panic!("stack overflow you dumb fucker");
+            panic!("stack overflow");
         }
 
         stack[sp] = self.position_in_memory as u16;
@@ -72,7 +86,7 @@ impl CPU {
 
     fn ret(&mut self) {
         if self.stack_pointer == 0 {
-            panic!("stack underflow (jesus christ you're way too dumb)");
+            panic!("stack underflow");
         }
 
         self.stack_pointer -= 1;
@@ -82,31 +96,25 @@ impl CPU {
 }
 
 fn main() {
-    let mut cpu = CPU {
-        registers: [0; 16],
-        memory: [0; 4096],
-        position_in_memory: 0,
-        stack: [0; 16],
-        stack_pointer: 0,
-    };
+    let mut cpu = CPU::new();
 
     cpu.registers[0] = 5;
     cpu.registers[1] = 10;
 
-    let mem = &mut cpu.memory;
-    mem[0x000] = 0x21;
-    mem[0x001] = 0x00;
-    mem[0x002] = 0x21;
-    mem[0x003] = 0x00;
-    mem[0x004] = 0x00;
-    mem[0x005] = 0x00;
+    let program: Vec<u8> = vec![
+        0x21, 0x00, // CALL 0x100
+        0x21, 0x00, // CALL 0x100
+        0x00, 0x00, // NOP
+        0x80, 0x14, // ADD V0, V1
+        0x80, 0x14, // ADD V0, V1
+        0x00, 0xEE, // RET
+    ];
 
-    mem[0x100] = 0x80;
-    mem[0x101] = 0x14;
-    mem[0x102] = 0x80;
-    mem[0x103] = 0x14;
-    mem[0x104] = 0x00;
-    mem[0x105] = 0xEE;
+    // load program from address 0x000 (start)
+    cpu.load_program(&program[0..6], 0x000);
+
+    // load subroutine of memory from address 0x100
+    cpu.load_program(&program[6..], 0x100);
 
     cpu.run();
 
